265ff143c3ff34b8d02298837d70284086c156fd
sowa-take <souwasora@gmail.com>
2022-03-12 23:37:32 +0900

    Lesson1完成

--- php_loop/Lesson1.php
+++ php_loop/Lesson1.php
@@ -1,1 +1,10 @@
 <?php
+for ($i = 0; $i <= 321; $i++) {
+    if ($i == 1) {
+        echo "$i<br />";
+    } elseif ($i == 21) {
+        echo "$i<br />";
+    } elseif ($i == 321) {
+        echo "$i<br />";
+    }
+}
