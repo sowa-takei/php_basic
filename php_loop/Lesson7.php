37749ab73f27c67bddc1d3137d264753aa2f903a
sowa-take <souwasora@gmail.com>
2022-03-14 22:42:57 +0900

    少し修正

--- php_loop/Lesson7.php
+++ php_loop/Lesson7.php
@@ -6,7 +6,7 @@ for ($i =1; $i <= 4; $i++) {
     for ($k = 1; $k <= $i; $k++) {
         echo $k;
     }
-    for ($l = 3; $l >=6-$k; $l--) {
+    for ($l = 3; $l >= 6 - $k; $l--) {
         echo $l-(4-$i);
     }
     echo "<br />";
