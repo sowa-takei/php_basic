f88ff34dc5baf0f8917a09e341860c3523cdc2f9
sowa-take <souwasora@gmail.com>
2022-03-16 11:48:52 +0900

    Lesson8完成

--- php_loop/Lesson8.php
+++ php_loop/Lesson8.php
@@ -1,12 +1,8 @@
 <?php
-for ($i = 0; $i < 2; $i++) {
-    for ($a = 0; $a < ($i + 1)*2-1; $a++) {
-        echo  "*";
-        for ($j = 3; $j >= 1; $j--) {
-            for ($k = 1; $k <= $j; $k++) {
-                echo $k;
-            }
-                echo "<br />";
-        }
+for ($j = 3; $j >= 1; $j--) {
+    echo str_repeat('*', 3-$j);
+    for ($k = 1; $k <= $j; $k++) {
+        echo $k;
     }
+    echo "<br />";
 }
